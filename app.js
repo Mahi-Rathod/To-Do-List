1d761ef5743fcc68951fcf9817630e0bd776ede3
Mahi-Rathod <s20_rathod_mahesh@mgmcen.ac.in>
2024-10-21 17:11:50 +0530

    Created the Add new task container for adding new task with required input field, date and time

--- app.js
+++ app.js
@@ -8,3 +8,33 @@ heading.innerText = "To-Do List";
 
 headingContainer.appendChild(heading);
 container.appendChild(headingContainer);
+
+//Container for the Add new Task
+
+const addNewTaskForm = document.createElement("form");
+addNewTaskForm.classList.add("add-new-task");
+
+container.appendChild(addNewTaskForm);
+
+const inputForTask = document.createElement("input");
+inputForTask.type = "text";
+inputForTask.placeholder = "Add your task here";
+inputForTask.required = true;
+addNewTaskForm.appendChild(inputForTask);
+
+const date = document.createElement("input");
+date.type = "date";
+date.required = true;
+addNewTaskForm.appendChild(date);
+
+const time = document.createElement("input");
+time.required = true;
+time.type = "time";
+
+addNewTaskForm.appendChild(time);
+
+const addTaskBtn = document.createElement("button");
+addTaskBtn.innerText = "Add Task";
+addTaskBtn.type = 'submit';
+
+addNewTaskForm.appendChild(addTaskBtn);
